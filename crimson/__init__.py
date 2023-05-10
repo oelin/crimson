0c582a440fd581f3361b52fbc60b5345c0c89d8a
kale <42823429+oelin@users.noreply.github.com>
2023-05-10 14:36:49 +0100

    Update __init__.py

--- crimson/__init__.py
+++ crimson/__init__.py
@@ -3,24 +3,12 @@ from random import random
 from py_compile import compile
 
 
-def invalidate(path: str, code: str) -> None:
-
-    # Create a temporary file...
-
+def poison(path: str, code: str) -> None:
+    
     temporary = str(random())
-
-    # Move the module into the temporary file...
-
     rename(path, temporary)
-
-    # Create a poisoned module...
-
+    
     open(path, "w").write(code)
-
-    # Compile it with unchecked invalidation...
-
     compile(path, invalidation_mode=3)
-
-    # Move the original module back...
-
+    
     rename(temporary, path)
